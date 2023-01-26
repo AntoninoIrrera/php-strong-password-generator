5a1a9f126be6f8b0ed969b2aacd77400d92b7067
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-26 15:40:45 +0100

    possibilita di scegliere se ripetere  o meno i caratteri selezionati

--- function.php
+++ function.php
@@ -1,38 +1,88 @@
 <?php
-    function getRandomPass($lunghezzaPass,$lettere,$numeri,$simboli)
+    function getRandomPass($lunghezzaPass,$lettere,$numeri,$simboli,$ripetizione)
     {
         $i = 0;
         $password = "";
+        $caratteriPassList = [];
+
 
         while ($i < $lunghezzaPass) {
 
-            if($lettere == 1){
+
+            if($ripetizione == 0){
+
+
+                if ($lettere == 1) {
+                    $randomLetter = chr(97 + mt_rand(0, 25));
+
+                    if(!in_array($randomLetter,$caratteriPassList)){
+                        $caratteriPassList[] = $randomLetter;
+                        $i++;
+                    }
+
+                    if ($i < $lunghezzaPass) {
+                        $randomCapsLetter = chr(65 + mt_rand(0, 25));
+                        if(!in_array($randomCapsLetter,$caratteriPassList)){
+                            $caratteriPassList[] = $randomCapsLetter;
+                            $i++;
+                        }
+                    }
+                }
+                if ($numeri == 1) {
+                    if ($i < $lunghezzaPass) {
+                        $randomNumber = mt_rand(0, 9);
+                        if(!in_array($randomNumber,$caratteriPassList)){
+                            $caratteriPassList[] = $randomNumber;
+                            $i++;
+                        }
+                    }
+                }
+                if ($simboli == 1) {
+                    if ($i < $lunghezzaPass) {
+                        $randomSimbol = chr(33 + mt_rand(0, 14));
+                        if(!in_array($randomSimbol,$caratteriPassList)){
+                            $caratteriPassList[] = $randomSimbol;
+                            $i++;
+                        }
+                    }
+                }
+
+            }else{
+
+                
+                if($lettere == 1){
                 $password .= chr(97 + mt_rand(0, 25));
                 $i++;
                 if($i < $lunghezzaPass){
                     $password .= chr(65 + mt_rand(0, 25));
                     $i++;
                 }
-            }
-            if($numeri == 1){
-                if($i < $lunghezzaPass){
-                    $password .= mt_rand(0, 9);
-                    $i++;
                 }
-            }
-            if($simboli == 1){
-                if($i < $lunghezzaPass){
-                    $password .= chr(33 + mt_rand(0, 14));
-                    $i++;
+                if($numeri == 1){
+                    if($i < $lunghezzaPass){
+                        $password .= mt_rand(0, 9);
+                        $i++;
+                    }
                 }
-            }
+                if($simboli == 1){
+                    if($i < $lunghezzaPass){
+                        $password .= chr(33 + mt_rand(0, 14));
+                        $i++;
+                    }
+                }
             
             
-
-
+            }
 
 
         }
+
+        if(count($caratteriPassList) > 0){
+            foreach ($caratteriPassList as $carattere) {
+                $password .= $carattere;
+            }
+        }
+
 
         return $password;
     }
--- index.php
+++ index.php
@@ -13,13 +13,18 @@
 
     <form action="index.php" method="GET">
         <label for="lunghezza">Lunghezza password</label>
-        <input type="number" name="lunghezza" id="lunghezza">
+        <input type="number" name="lunghezza" id="lunghezza"><br>
         <input type="checkbox" id="lettere" name="lettere" value="1">
         <label for="lettere"> Lettere</label>
         <input type="checkbox" id="numeri" name="numeri" value="1">
         <label for="vehicle2"> Numeri</label>
         <input type="checkbox" id="simboli" name="simboli" value="1">
-        <label for="simboli"> Simboli</label>
+        <label for="simboli"> Simboli</label><br>
+        <label for="si">Ripete caratteri</label>
+        <input type="radio" id="si" name="repeat" value="1">
+        <label for="si">Si</label>
+        <input type="radio" id="no" name="repeat" value="0">
+        <label for="no">No</label><br>
         <button type="submit">Invio</button>
     </form>
 
@@ -37,16 +42,20 @@
     <?php
 
     if (isset($_GET["lunghezza"])) {
-        if(isset($_GET["lettere"]) or isset($_GET["numeri"]) or isset($_GET["simboli"])){
+        if (isset($_GET["lettere"]) or isset($_GET["numeri"]) or isset($_GET["simboli"])) {
+            if(isset($_GET["repeat"])){
 
-            if ($_GET["lunghezza"] >= 8) {
-                // echo "password: " . getRandomPass($_GET["lunghezza"]);
-                $_SESSION['password'] = getRandomPass($_GET["lunghezza"],$_GET["lettere"],$_GET["numeri"],$_GET["simboli"]);
-                header("location: ./login.php");
-            } else {
-                echo "la password deve contenere aleno 8 caratteri";
+                if ($_GET["lunghezza"] >= 8) {
+                    // echo "password: " . getRandomPass($_GET["lunghezza"]);
+                    $_SESSION['password'] = getRandomPass($_GET["lunghezza"], $_GET["lettere"], $_GET["numeri"], $_GET["simboli"], $_GET["repeat"]);
+                    header("location: ./login.php");
+                } else {
+                    echo "la password deve contenere aleno 8 caratteri";
+                }
+            }else{
+                echo "selezionare se ripete o meno i caratteri";
             }
-        }else{
+        } else {
             echo "selezionare almeno un tipo di carattere per la password";
         }
     }
