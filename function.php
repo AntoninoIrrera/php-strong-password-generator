554fd7cbf268b738869caed34bc7754626460d83
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-26 15:12:31 +0100

    possibilità di scegliere se inserire nella password generata lettere simboli o numeri

--- function.php
+++ function.php
@@ -1,24 +1,30 @@
 <?php
-    function getRandomPass($lunghezzaPass)
+    function getRandomPass($lunghezzaPass,$lettere,$numeri,$simboli)
     {
         $i = 0;
         $password = "";
 
         while ($i < $lunghezzaPass) {
 
-            $password .= chr(97 + mt_rand(0, 25));
-            $i++;
-            if($i < $lunghezzaPass){
-                $password .= chr(65 + mt_rand(0, 25));
+            if($lettere == 1){
+                $password .= chr(97 + mt_rand(0, 25));
                 $i++;
+                if($i < $lunghezzaPass){
+                    $password .= chr(65 + mt_rand(0, 25));
+                    $i++;
+                }
             }
-            if($i < $lunghezzaPass){
-                $password .= mt_rand(0, 9);
-                $i++;
+            if($numeri == 1){
+                if($i < $lunghezzaPass){
+                    $password .= mt_rand(0, 9);
+                    $i++;
+                }
             }
-            if($i < $lunghezzaPass){
-                $password .= chr(33 + mt_rand(0, 14));
-                $i++;
+            if($simboli == 1){
+                if($i < $lunghezzaPass){
+                    $password .= chr(33 + mt_rand(0, 14));
+                    $i++;
+                }
             }
             
             
--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
 </head>
+
 <body>
-    
+
 
     <form action="index.php" method="GET">
         <label for="lunghezza">Lunghezza password</label>
         <input type="number" name="lunghezza" id="lunghezza">
+        <input type="checkbox" id="lettere" name="lettere" value="1">
+        <label for="lettere"> Lettere</label>
+        <input type="checkbox" id="numeri" name="numeri" value="1">
+        <label for="vehicle2"> Numeri</label>
+        <input type="checkbox" id="simboli" name="simboli" value="1">
+        <label for="simboli"> Simboli</label>
         <button type="submit">Invio</button>
     </form>
 
 
     <?php
 
-        include_once __DIR__ . "/function.php";
+    include_once __DIR__ . "/function.php";
 
-        session_start();
+    session_start();
 
     ?>
 
@@ -28,15 +36,20 @@
 
     <?php
 
-        if(isset($_GET["lunghezza"])){
-            if($_GET["lunghezza"] >= 8){
+    if (isset($_GET["lunghezza"])) {
+        if(isset($_GET["lettere"]) or isset($_GET["numeri"]) or isset($_GET["simboli"])){
+
+            if ($_GET["lunghezza"] >= 8) {
                 // echo "password: " . getRandomPass($_GET["lunghezza"]);
-                $_SESSION['password'] = getRandomPass($_GET["lunghezza"]);
+                $_SESSION['password'] = getRandomPass($_GET["lunghezza"],$_GET["lettere"],$_GET["numeri"],$_GET["simboli"]);
                 header("location: ./login.php");
-            }else{
+            } else {
                 echo "la password deve contenere aleno 8 caratteri";
             }
+        }else{
+            echo "selezionare almeno un tipo di carattere per la password";
         }
+    }
     ?>
 
 
@@ -45,4 +58,5 @@
 
 
 </body>
+
 </html>
