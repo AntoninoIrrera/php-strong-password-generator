f8643fb760cad3dcbc81f6770485f7ff096a862e
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-26 14:28:10 +0100

    generatore password con caratteri minuscoli/maiuscoli numeri e simboli in base alla lunghezza scelta dall'utente

--- index.php
+++ index.php
@@ -9,8 +9,47 @@
 <body>
     
 
+    <form action="index.php" method="GET">
+        <label for="lunghezza">Lunghezza password</label>
+        <input type="number" name="lunghezza" id="lunghezza">
+        <button type="submit">Invio</button>
+    </form>
+
+
+
+
+
+
     <?php
 
+
+        function getRandomPass($lunghezzaPass){
+            $i = 0;
+            $password = "";
+
+            while($i < $lunghezzaPass){
+
+                $password .= chr(97 + mt_rand(0, 25));
+                $password .= chr(65 + mt_rand(0, 25));
+                $password .= mt_rand(0, 9);
+                $password .= chr(33 + mt_rand(0, 14));
+                
+
+
+                
+                $i = $i + 4;
+            }
+                
+            return $password;
+        }
+
+        if(isset($_GET["lunghezza"])){
+            if($_GET["lunghezza"] >= 4){
+                echo "password: " . getRandomPass($_GET["lunghezza"]);
+            }else{
+                echo "la password deve contenere aleno 4 caratteri";
+            }
+        }
     ?>
 
 
